1bca6cb3b7fc2bc68bf2d30270bbaa0c8e2055a2
PraveenNarwal <praveennarwal5@gmail.com>
2022-10-09 11:39:52 +0530

    post and delete category

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,11 +1,59 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countStock: {
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    richDescription: {
+        type: String,
+        default: '',
+    },
+    image: {
+        type: String,
+        default: '',
+    },
+    images: [{
+        type: String,
+    }],
+    brand: {
+        type: String,
+        default: '',
+    },
+    price: {
         type: Number,
+        default: 0,
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
         required: true
+    },
+    countInStock: {
+        type: String,
+        required: true,
+        min: 0,
+        max: 255
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    numReview:{
+        type: Number,
+        default: 0
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+    dateCreated: {
+        type: String,
+        default: Date.now
     },
 })
 
