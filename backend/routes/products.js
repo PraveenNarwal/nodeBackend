44b73f45a03ee171865197e76e5bc02e3b1ab1c6
PraveenNarwal <praveennarwal5@gmail.com>
2022-10-09 14:14:37 +0530

    create product if category has it

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,27 +1,44 @@
+const { Category } = require("../models/category");
 const { Product } = require("../models/product");
 const express = require("express");
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-  const products = await Product.find();
+    const products = await Product.find();
 
-  if (!products) {
-    res.status(500).json({ success: false });
-  }
-  res.send(products);
+    if (!products) {
+        res.status(500).json({ success: false });
+    }
+    res.send(products);
 });
 
-router.post(`/`, (req, res) => {
-  const products = new Product(req.body);
+router.post(`/`, async (req, res) => {
 
-  products
-    .save()
-    .then((createdproduct) => {
-      res.status(201).json(createdproduct);
-    })
-    .catch((err) => {
-      res.status(500).json(err);
+    const category = await Category.findById(req.body.category);
+    if (!category) {
+        return res.status(400).send('invalid category');
+    }
+
+    const products = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReview: req.body.numReview,
+        isFeatured: req.body.isFeatured,
+
     });
+    await products.save();
+    if (!products) {
+        return res.status(500).send('the product cannot be created');
+    }
+    res.send(products);
+
 });
 
 module.exports = router;
