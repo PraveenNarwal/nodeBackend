76451311bae3416a813dc8e83b0e5012019dbe65
PraveenNarwal <praveennarwal5@gmail.com>
2022-10-09 13:07:40 +0530

    get by id and update the database

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -19,6 +19,9 @@ router.get('/:id', async (req, res) => {
     res.status(200).send(category);
 })
 
+
+
+
 router.post('/', async (req, res)=>{
     let category = new Category({
         name: req.body.name,
@@ -34,14 +37,44 @@ router.post('/', async (req, res)=>{
     res.send(category);
 })
 
-router.delete('/:id', (req, res) => {
-    Category.findByIdAndRemove(req.params.id).then(category => {
-        if(category){
-            return res.status(200).json({success: true, message: 'the category deleted'})
-        }else{
-            return res.status(404).json({success: false, message: 'not found the category'})
-        }
-    }).catch(err => {return res.status(400).json({success: false, error: err})})
-})
+
+router.put("/:id", async (req, res) => {
+    const cateUpdate ={
+        name: req.body.name,
+        color: req.body.color,
+        icon: req.body.icon,
+      };
+    const category = await Category.findByIdAndUpdate(
+      req.params.id,
+      cateUpdate,
+      { new: true,   
+         }
+    )
+    if (!category) {
+       return res
+        .status(400)
+        .json({ success: false, message: "No category found by id" });
+    }
+    res.send(category)
+
+  });
+
+router.delete("/:id", (req, res) => {
+  Category.findByIdAndRemove(req.params.id)
+    .then((category) => {
+      if (category) {
+        return res
+          .status(200)
+          .json({ success: true, message: "the category deleted" });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: "not found the category" });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
+});
 
 module.exports = router;
